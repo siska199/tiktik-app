a434c30319ff4d70f023dde6f4f8d44bac054b20
siska199 <siska.apriana.1703226@students.um.ac.id>
2022-07-03 06:59:45 +0700

    initial setup next and sanity as the cms

---
+++ tiktik-app/schemas/comment.js
@@ -0,0 +1,18 @@
+export default {
+    name: "comment",
+    titile: "comment",
+    type: document,
+    fields: [
+      {
+        name: "postedBy",
+        title: "comment name",
+        type: "string",
+      },
+      {
+        name: "image",
+        title: "image",
+        type: "string",
+      },
+    ],
+  };
+  
---
+++ tiktik-app/schemas/user.js
@@ -0,0 +1,17 @@
+export default {
+  name: "user",
+  titile: "User",
+  type: document,
+  fields: [
+    {
+      name: "username",
+      title: "Username",
+      type: "string",
+    },
+    {
+      name: "image",
+      title: "image",
+      type: "string",
+    },
+  ],
+};
