92e676c5a744fbaff03410c3e95fb593e8a8bf08
siska199 <siska.apriana.1703226@students.um.ac.id>
2024-06-27 07:16:09 +0700

    update store

--- redux/store.ts
+++ redux/store.ts
@@ -2,21 +2,21 @@ import {createStore, applyMiddleware,compose} from "redux"
 import thunk from "redux-thunk"
 import reducers from "./reducers"
 
-declare global {
-    interface Window {
-      __REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : typeof compose;
-    }
-}
+// declare global {
+//     interface Window {
+//       __REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : typeof compose;
+//     }
+// }
 
-let composeEnhancers : Function | any = compose
-if(typeof window !=="undefined"){
-    composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-        trace: true,
-        traceLimit: 25,
-    }) || compose
-}
-
-const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)))
+// let composeEnhancers : Function | any = compose
+// if(typeof window !=="undefined"){
+//     composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+//         trace: true,
+//         traceLimit: 25,
+//     }) || compose
+// }
+//composeEnhancers(applyMiddleware(thunk))
+const store = createStore(reducers, applyMiddleware(thunk))
 
 export type AppDispatch = typeof store.dispatch; // Here we export the store's dispatch type
 export type RootState = ReturnType<typeof store.getState>; // Here we export the store's state
